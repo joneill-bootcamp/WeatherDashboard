07a25346f215e039feeaf865d16a5df84dc41e12
John O'Neil <fluffywestie@tpg.com.au>
2020-03-14 23:54:37 +1100

    Added all API calls

--- script.js
+++ script.js
@@ -32,6 +32,11 @@ function renderRecentCities() {
         // Add text of City Name
         aButton.text(element);
 
+        // Add an On click event to the button
+        aButton.on("click", function () {
+            renderWeather($(this).attr("data-city"));
+        });
+
         // Append to the list item
         aListItem.append(aButton);
 
@@ -40,13 +45,57 @@ function renderRecentCities() {
     });
 }
 
-// Set up search of click event on search box
 
+function renderWeather(city) {
+
+    // Call weatherQueryURL
+    $.ajax({
+            url: weatherQueryURL + city + "&appid=" + APIkey + "&units=metric",
+            method: "GET"
+
+        }).then(function (response) {
+            console.log("Current");
+            console.log(response);
+            var coordinates = response.coord;
+            // Call uviQueryURL
+            $.ajax({
+                    url: uviQueryURL + city + "&appid=" + APIkey + "&units=metric" + "&lat=" + coordinates.lat + "&lon=" + coordinates.lon,
+                    method: "GET"
+
+                }).then(function (response) {
+                    console.log("UVI ");
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log("Got error");
+                    console.log(error);
+                });
+        })
+        .catch(function (error) {
+
+        });
+
+    // Call forecastQueryURL
+    $.ajax({
+            url: forecastQueryURL + city + "&appid=" + APIkey + "&units=metric",
+            method: "GET"
+
+        }).then(function (response) {
+            console.log("5 day");
+            console.log(response);
+        })
+        .catch(function (error) {
+
+        });
+}
+
+// Set up search of click event on search box
 $("#searchButton").on("click", function () {
 
     // Call the API's and get the data
     let city = $("#citySearch").val();
     $.ajax({
+            // Query the Weather URL, check if city is a valid city
             url: weatherQueryURL + city + "&appid=" + APIkey + "&units=metric",
             method: "GET"
         })
@@ -57,9 +106,18 @@ $("#searchButton").on("click", function () {
             // Render list of buttons
             renderRecentCities();
 
+            // Render Weather data
+            renderWeather(city);
+
             console.log(response);
         })
         .catch(function (error) {
-            alert("got an error!");
+            if (error.status === 404) {
+                // City name forms part of URL, therefore alert user if they entered invalid city name
+                // Because this is a URL parameter, an invalid city name will generate a 404 error (Resource not found)
+                alert("That city was not found, please check spelling and try again");
+            }
+            // Write error object to log
+            console.log(error);
         });
 });
